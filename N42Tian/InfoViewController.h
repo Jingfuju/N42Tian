b22b8f6a63092a109b652f753bac0c05190eb90a
hhsolar <hongh08@gmail.com>
2017-01-11 17:57:08 -0800

    change tab bar name

---
+++ N42Tian/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  N42Tian
+//
+//  Created by apple on 11/1/2017.
+//  Copyright © 2017 N42Tian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
