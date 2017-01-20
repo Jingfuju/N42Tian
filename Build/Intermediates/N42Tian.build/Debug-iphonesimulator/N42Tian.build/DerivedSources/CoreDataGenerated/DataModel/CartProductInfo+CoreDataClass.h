7cc48b1241957474a6bdd0e43d6fd6a3123bf889
hhsolar <hongh08@gmail.com>
2017-01-20 15:29:19 -0800

    additional commit

---
+++ Build/Intermediates/N42Tian.build/Debug-iphonesimulator/N42Tian.build/DerivedSources/CoreDataGenerated/DataModel/CartProductInfo+CoreDataClass.h
@@ -0,0 +1,21 @@
+//
+//  CartProductInfo+CoreDataClass.h
+//  
+//
+//  Created by apple on 20/1/2017.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface CartProductInfo : NSManagedObject
+
+@end
+
+NS_ASSUME_NONNULL_END
+
+#import "CartProductInfo+CoreDataProperties.h"
---
+++ Build/Intermediates/N42Tian.build/Debug-iphonesimulator/N42Tian.build/DerivedSources/CoreDataGenerated/DataModel/DataModel+CoreDataModel.h
@@ -0,0 +1,17 @@
+//
+//  DataModel+CoreDataModel.h
+//  
+//
+//  Created by apple on 20/1/2017.
+//
+//  This file was automatically generated and should not be edited.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+#import "CartProductInfo+CoreDataClass.h"
+
+
+
+
